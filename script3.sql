d5eaacf8d421fa7a071432603c50cd04a690b2bd
Ankit Arora <ankitarora@Ankit.local>
2025-10-16 16:14:15 +0530

    sql

---
+++ script3.sql
@@ -0,0 +1,5 @@
+INSERT INTO conversation_metadata (client_config_id, product_id, assistant_id, gender, asr_vendor, asr_lang_code, asr_lang_name, asr_model_name, tts_lang_code, tts_provider, tts_speed, tts_voice_name, tts_style, tts_resp_format, llm_model, translate, additional_info, created_at, updated_at)
+VALUES
+(1, 'prod001', 1, 'male', 'azure', 'en-US', 'English (US)', 'nova-2', 'en-US', 'azure', 1, 'en-US-AvaNeural', 'chat', 'base64', 'gpt-4o-mini', 0, '', '2025-05-15 08:17:05', '2025-09-16 06:39:59'),
+(2, 'prod001', 1, 'male', 'azure', 'en-US', 'English (US)', 'nova-2', 'en-US', 'azure', 1, 'en-US-AvaNeural', 'chat', 'base64', 'gpt-4o-mini', 0, '', '2025-05-15 08:17:05', '2025-09-19 00:25:48');
+
